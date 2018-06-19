1c04d2269b77ef3f0da767100bd4f033422ab5e4
Patryk Konior <patryk@arteia.co>
2018-06-19 20:18:40 +0200

    npm'ied the project

--- truffle.js
+++ truffle.js
@@ -1,4 +1,37 @@
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const { 
+  secretMnemonic = '',
+} = require('./secrets');
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+    networks: {
+        live: {
+            network_id: "*"
+        },
+        ropsten: {
+            provider: new HDWalletProvider(secretMnemonic, 'https://ropsten.infura.io/'),
+            network_id: '3'
+        },
+        testrpc: {
+            network_id: 'default'
+        },
+        truffle_develop: {
+          port: 8545,
+          host: "localhost",
+          network_id: "4447",
+        },
+        coverage: {
+            host: "localhost",
+            network_id: "*",
+            port: 8545,
+            gas: 0xfffffffffff,
+            gasPrice: 0x01
+        },
+        development: {
+            port: 8545,
+            host: "localhost",
+            network_id: "*",
+        },
+    }
+}
